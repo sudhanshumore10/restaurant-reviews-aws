1aeaf07c03758785e5c17bcb16d75aa4da61b5d6
sudhanshumore10 <123857944+sudhanshumore10@users.noreply.github.com>
2025-11-29 00:38:37 +0530

    Update dynamodb.ts

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -1,13 +1,12 @@
-// lib/dynamodb.ts
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-  },
+region: process.env.APP_REGION,
+credentials: {
+accessKeyId: process.env.APP_ACCESS_KEY_ID as string,
+secretAccessKey: process.env.APP_SECRET_ACCESS_KEY as string,
+},
 });
 
 export const ddb = DynamoDBDocumentClient.from(client);
